bb1a8188839dfbc71f3f5d856647c3f86ad30325
LeoHongboWANG <139325785+LeoHongboWANG@users.noreply.github.com>
2023-11-17 12:37:51 +0800

    Update trim.py

--- src/trim.py
+++ src/trim.py
@@ -1,1 +1,44 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+def main(input_file, cm_cutoff, edge_percentage, output_dir):
+    # Read input file
+    lgfile = pd.read_csv(input_file, sep='\t', comment='#', header=None)
+    
+    # Setup output file names and directories
+    create_output_directories(output_dir)
+    
+    # Prune the ends of linkage groups
+    pruned_lgfile = prune_linkage_groups(lgfile, cm_cutoff, edge_percentage)
+    
+    # Generate plots and save them
+    create_plots(pruned_lgfile, output_dir)
+    
+    # Output filtered files and log files
+    output_filtered_files(pruned_lgfile, output_dir)
+
+def create_output_directories(output_dir):
+    os.makedirs(os.path.join(output_dir, "plots"), exist_ok=True)
+    os.makedirs(os.path.join(output_dir, "logs"), exist_ok=True)
+    os.makedirs(os.path.join(output_dir, "QC_raw"), exist_ok=True)
+
+def prune_linkage_groups(lgfile, cm_cutoff, edge_percentage):
+    # Implement pruning logic here
+    return pruned_lgfile
+
+def create_plots(pruned_lgfile, output_dir):
+    # Implement plotting logic here
+
+def output_filtered_files(pruned_lgfile, output_dir):
+    # Implement file output logic here
+
+if __name__ == "__main__":
+    input_file = "input_file_path"
+    cm_cutoff = 0.1
+    edge_percentage = 0.1
+    output_dir = "output_directory"
+    
+    main(input_file, cm_cutoff, edge_percentage, output_dir)
