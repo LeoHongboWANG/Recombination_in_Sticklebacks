23bf94c219c259cc3728487828212880bafa64e0
LeoHongboWANG <139325785+LeoHongboWANG@users.noreply.github.com>
2026-01-27 14:00:54 +0800

    Update process_files.sh

--- src/process_files.sh
+++ src/process_files.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Run the get_pedigree.py script on the Parentage_8pops.csv file
+# Run the get_pedigree.py 
 python get_pedigree.py Parentage_8pops.csv
 
-# Loop over all files ending in _output.txt
+# Loop over all file
 for file in *_output.txt
 do
-    # Extract part of the filename to use as the directory name
+   
     dir_name=$(echo $file | cut -d "_" -f1)
     
-    # Create a new directory (if it doesn't already exist)
-    mkdir -p $dir_name
     
-    # Move the file into the new directory
+    mkdir -p $dir_name
+
     mv $file $dir_name/
 
-    # Extract the second line from the file and save it to sorted_bams
     awk 'NR==2{for(i=3; i<=NF; i++) print $i}' $dir_name/$file > $dir_name/sorted_bams
 done
